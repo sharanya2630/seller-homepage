2eaff3811a7a4159157613fe14120369a0980fbc
KARRE SHARANYA <112693767+sharanya2630@users.noreply.github.com>
2024-12-06 10:05:11 +0530

    first commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,108 +1,3 @@
-// import React, { useState } from "react";
-// import "./Header.scss";
-
-// const Header = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   return (
-//     <header className="navbar">
-//       <nav className="navbar-container">
-//         <div className="logo">
-//           <img
-//             src="https://sellerscommerce.com/wp-content/uploads/2023/04/logo104909.png"
-//             alt="SellersCommerce Logo"
-//           />
-//         </div>
-//         <div className="hamburger" onClick={toggleMenu}>
-//           <span></span>
-//           <span></span>
-//           <span></span>
-//         </div>
-//         <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-//           <li className="dropdown">
-//             <a href="#platform" className="nav-item">
-//               Platform
-//             </a>
-//             <ul className="dropdown-menu">
-//               <li>
-//                 <a href="#overview">Overview</a>
-//               </li>
-//               <li>
-//                 <a href="#wholesale">Wholesale Commerce</a>
-//               </li>
-//               <li>
-//                 <a href="#retail">Retail Commerce</a>
-//               </li>
-//               <li>
-//                 <a href="#multistore">Multistore Ecommerce</a>
-//               </li>
-//               <li>
-//                 <a href="#white-label">White Label Ecommerce</a>
-//               </li>
-//               <li>
-//                 <a href="#company-stores">Company Stores</a>
-//               </li>
-//               <li>
-//                 <a href="#catalog">Catalog as a Service</a>
-//               </li>
-//             </ul>
-//           </li>
-//           <li className="dropdown">
-//             <a href="#industries" className="nav-item">
-//               Industries
-//             </a>
-//             <ul className="dropdown-menu">
-//               <li>
-//                 <a href="#overview">Overview</a>
-//               </li>
-//               <li>
-//                 <a href="#wholesale">Wholesale Commerce</a>
-//               </li>
-//               <li>
-//                 <a href="#retail">Retail Commerce</a>
-//               </li>
-//             </ul>
-//           </li>
-//           <li>
-//             <a href="#integrations" className="nav-item">
-//               Integrations
-//             </a>
-//           </li>
-//           <li className="dropdown">
-//             <a href="#industries" className="nav-item">
-//               Resources
-//             </a>
-//             <ul className="dropdown-menu">
-//               <li>
-//                 <a href="#overview">Overview</a>
-//               </li>
-//               <li>
-//                 <a href="#wholesale">Wholesale Commerce</a>
-//               </li>
-//               <li>
-//                 <a href="#retail">Retail Commerce</a>
-//               </li>
-//             </ul>
-//           </li>
-//           <li>
-//             <a href="#contact" className="nav-item">
-//               Contact
-//             </a>
-//           </li>
-//           <li className="demo">
-//             <a className="request-demo">Request Demo</a>
-//           </li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
 
 import React, { useState,useEffect } from "react";
 
@@ -112,12 +7,11 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   useEffect(() => {
     if (menuOpen) {
-      document.body.style.overflow = "hidden"; // Disable scrolling
+      document.body.style.overflow = "hidden"; 
     } else {
-      document.body.style.overflow = "auto"; // Re-enable scrolling
+      document.body.style.overflow = "auto"; 
     }
 
-    // Clean up the effect when component unmounts or menu is closed
     return () => {
       document.body.style.overflow = "auto";
     };
